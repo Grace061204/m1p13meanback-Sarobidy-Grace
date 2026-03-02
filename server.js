2be74c03e0faf2b44a7288537da17a4f17d21df1
Hariniaina-Grace <grace.andrianisoa@gmail.com>
2026-03-02 20:18:21 +0300

    rectification import

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 require('./models/article/Article');
-require('./models/vente/Vente');
+require('./models/Vente/Vente');
 require('./models/detailVente/DetailVente');
 require('./models/mouvementStock/MouvementStock');
 require('./models/historiquePrix/HistoriquePrix');
